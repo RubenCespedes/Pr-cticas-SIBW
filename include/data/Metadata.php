383371731135eaa1c68aa886e0573b129f1b1c74
caentrena <caentrena@gmail.com>
2017-04-18 04:21:11 +0200

    metada actualizado

--- include/data/Metadata.php
+++ include/data/Metadata.php
@@ -11,16 +11,33 @@ class Metadata {
         global $sec;
 
         switch($sec) {
-            case 'noticia_impresa':
-                $this->description = 'Noticia impresa del Hotel Plaza Nueva en Granada';
-                $this->keywords = 'Noticia impresa Periodico, Noticia impresa Periodico Granada, Periodico impreso Granada, Periodico impreso España, Periodico impreso';
-                $this->title = 'Noticia Periodico El Profeta';
+            case 'nacional':
+                $this->description = 'Noticia nacional del Periodico El Profeta';
+                $this->keywords = 'Noticia nacional Periodico, Noticia nacional Periodico Granada, Periodico nacional Granada, Periodico nacional España, Periodico nacional';
+                $this->title = 'Noticia nacional Periodico El Profeta';
                 break;
-            case 'noticia':
-                $this->description = 'Noticia del Hotel Plaza Nueva en Granada';
-                $this->keywords = 'Noticia Periodico, Noticia Periodico Granada, Periodico Granada, Periodico España, Periodico';
-                $this->title = 'Noticia Periodico El Profeta';
+            case 'internacional':
+                $this->description = 'Noticia internacional del Periodico El Profeta';
+                $this->keywords = 'Noticia internacional Periodico, Noticia internacional Periodico Granada, Periodico internacional Granada, Periodico internacional España, Periodico internacional';
+                $this->title = 'Noticia internacional Periodico El Profeta';
                 break;
+            case 'politica':
+                $this->description = 'Noticia politica del Periodico El Profeta';
+                $this->keywords = 'Noticia politica Periodico, Noticia politica Periodico Granada, Periodico politica Granada, Periodico politica España, Periodico politica';
+                $this->title = 'Noticia politica Periodico El Profeta';
+                break;
+            case 'deportes':
+                $this->description = 'Noticia deportes del Periodico El Profeta';
+                $this->keywords = 'Noticia deportes Periodico, Noticia deportes Periodico Granada, Periodico deportes Granada, Periodico deportes España, Periodico deportes';
+                $this->title = 'Noticia deportes Periodico El Profeta';
+                break;
+            case 'tecnologia':
+                $this->description = 'Noticia tecnologia del Periodico El Profeta';
+                $this->keywords = 'Noticia tecnologia Periodico, Noticia tecnologia Periodico Granada, Periodico tecnologia Granada, Periodico tecnologia España, Periodico tecnologia';
+                $this->title = 'Noticia tecnologia Periodico El Profeta';
+                break;
+
+
             default:
                 $this->description = 'Página principal del periodico El Profeta';
                 $this->keywords = 'Periodico Granada, Periodico España, Periodico';
